f527f76379a853875e190fb6da77f1d5599e7214
mmravec <martinmravec4@gmail.com>
2016-11-26 08:49:09 +0100

    Initial commit

---
+++ static/js.js
@@ -0,0 +1,19 @@
+/**
+ * Created by martinmravec on 20.11.16.
+ */
+$(function() {
+    $('#btnSignUp').click(function() {
+
+        $.ajax({
+            url: '/signUp',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
